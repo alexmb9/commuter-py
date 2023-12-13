37b610c4617317e075336f3bd85a53c7a07fd868
Alex Baxter <alex.baxter99@outlook.com>
2023-12-13 22:24:02 +0000

    data format changed into json for api request

--- commuter/commuter_main.py
+++ commuter/commuter_main.py
@@ -1,5 +1,6 @@
 import config
 import asyncio
+import json
 from datetime import datetime
 
 from traveltimepy import Driving, Coordinates, TravelTimeSdk
@@ -7,11 +8,22 @@ from traveltimepy import Driving, Coordinates, TravelTimeSdk
 async def main():
     sdk = TravelTimeSdk(app_id=config.app_id, api_key=config.api_key)
     
-    results = await sdk.time_map_async(
-        coordinates=[Coordinates(lat=51.507609, lng=-0.128315), Coordinates(lat=51.517609, lng=-0.138315)],
-        arrival_time=datetime.now(),
-        transportation=Driving()
-    )
+    coordinates = [
+        Coordinates(lat=51.507609, lng=-0.128315),
+        Coordinates(lat=51.517609, lng=-0.138315)
+    ]
+
+    # Convert the dictionary to a JSON string
+    arrival_time = datetime.now().isoformat()
+    data = {
+        'coordinates': coordinates,
+        'arrival_time': arrival_time,
+        'transportation': Driving().dict()
+    }
+    json_data = json.dumps(data)
+
+    # Pass the JSON string to the method
+    results = await sdk.time_map_async(json_data)
     print(results)
 
 asyncio.run(main())
