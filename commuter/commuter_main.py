ba1d45b497ec024ba6bcffbc1d01640a6a29ffa0
Alex Baxter <alex.baxter99@outlook.com>
2023-12-13 23:33:19 +0000

    changed async method to distance matrix time filter

--- commuter/commuter_main.py
+++ commuter/commuter_main.py
@@ -1,32 +1,32 @@
-import config
 import asyncio
 from datetime import datetime
-import json
+
+from traveltimepy import Location, Coordinates, PublicTransport, Property, FullRange, TravelTimeSdk
 
 from traveltimepy import Driving, Coordinates, TravelTimeSdk
 
 async def main():
     sdk = TravelTimeSdk(app_id=config.app_id, api_key=config.api_key)
     
-    coordinates = [
-        Coordinates(lat=51.507609, lng=-0.128315),
-        Coordinates(lat=51.517609, lng=-0.138315)
+    locations = [
+        Location(id="London center", coords=Coordinates(lat=51.508930, lng=-0.131387)),
+        Location(id="Hyde Park", coords=Coordinates(lat=51.508824, lng=-0.167093)),
+        Location(id="ZSL London Zoo", coords=Coordinates(lat=51.536067, lng=-0.153596))
     ]
 
-    arrival_time = datetime.now()
-    transportation = Driving()
+    results = await sdk.time_filter_async(
+        locations=locations,
+        search_ids={
+            "London center": ["Hyde Park", "ZSL London Zoo"],
+            "ZSL London Zoo": ["Hyde Park", "London center"],
+        },
+        departure_time=datetime.now(),
+        travel_time=3600,
+        transportation=PublicTransport(type="bus"),
+        properties=[Property.TRAVEL_TIME],
+        range=FullRange(enabled=True, max_results=3, width=600)
+    )
 
-    data = {
-        'coordinates': vars(coordinates),
-        'arrival_time': arrival_time,
-        'transportation': transportation
-    }
-
-    # Use the custom encoder for serialization
-    json_data = json.dumps(data)
-
-    # Pass the transportation as a separate argument
-    results = await sdk.time_map_async(json_data)
     print(results)
 
 asyncio.run(main())
